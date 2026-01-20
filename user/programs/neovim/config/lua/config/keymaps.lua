f1e21ccb32d86e8d0436c39ee4ee998161f91f06
jtawari095 <jtawari095@gmail.com>
2026-01-20 19:14:40 +0530

    dotfiles: user: neovim: set keybindings to move around buffer

--- user/programs/neovim/config/lua/config/keymaps.lua
+++ user/programs/neovim/config/lua/config/keymaps.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+-- Move around buffers
+vim.keymap.set('n', '<Tab>', '<CMD>bnext<CR>', { desc = 'Go to next buffer' })
+vim.keymap.set('n', '<S-Tab>', '<CMD>bprevious<CR>', { desc = 'Go to previous buffer' })
