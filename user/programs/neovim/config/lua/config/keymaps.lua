7904bed08e928e100602cfb9bf79c50f65195626
jtawari095 <jtawari095@gmail.com>
2026-01-26 20:27:57 +0530

    dotfiles: user: neovim: set keybindings to delete current buffer

--- user/programs/neovim/config/lua/config/keymaps.lua
+++ user/programs/neovim/config/lua/config/keymaps.lua
@@ -4,3 +4,4 @@ vim.g.maplocalleader = ' '
 -- Move around buffers
 vim.keymap.set('n', '<Tab>', '<CMD>bnext<CR>', { desc = 'Go to next buffer' })
 vim.keymap.set('n', '<S-Tab>', '<CMD>bprevious<CR>', { desc = 'Go to previous buffer' })
+vim.keymap.set('n', '<C-q>', '<CMD>bdelete<CR>', { desc = 'Delete current buffer' })
